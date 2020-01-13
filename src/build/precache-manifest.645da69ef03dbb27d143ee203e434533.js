a88570433a245e4ac911df53f0110d57350b218d
Arbaz Khan <arbaz708@yahoo.com>
2020-01-13 19:21:49 +0500

    First commit

---
+++ src/build/precache-manifest.645da69ef03dbb27d143ee203e434533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49278636b3a33dc47b33d654ee4abace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139b5f375743a5403e8a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "0f448a816696aad6aa5c",
+    "url": "/static/js/2.22525eeb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.22525eeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "139b5f375743a5403e8a",
+    "url": "/static/js/main.b2563aac.chunk.js"
+  },
+  {
+    "revision": "c59d8ddbc435ff2fc636",
+    "url": "/static/js/runtime-main.2bac53c8.js"
+  }
+]);
